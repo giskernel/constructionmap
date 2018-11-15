22d7d656add27dc993bf4cfd5873cc5c78df4076
giskernel <40665240+giskernel@users.noreply.github.com>
2018-11-16 01:56:44 +0530

    Add files via upload

--- vendor/swiftmailer/swiftmailer/lib/classes/Swift/MimePart.php
+++ vendor/swiftmailer/swiftmailer/lib/classes/Swift/MimePart.php
@@ -13,200 +13,47 @@
  *
  * @author Chris Corbyn
  */
-class Swift_Mime_MimePart extends Swift_Mime_SimpleMimeEntity
+class Swift_MimePart extends Swift_Mime_MimePart
 {
-    /** The format parameter last specified by the user */
-    protected $_userFormat;
-
-    /** The charset last specified by the user */
-    protected $_userCharset;
-
-    /** The delsp parameter last specified by the user */
-    protected $_userDelSp;
-
-    /** The nesting level of this MimePart */
-    private $_nestingLevel = self::LEVEL_ALTERNATIVE;
-
     /**
-     * Create a new MimePart with $headers, $encoder and $cache.
+     * Create a new MimePart.
      *
-     * @param Swift_Mime_HeaderSet      $headers
-     * @param Swift_Mime_ContentEncoder $encoder
-     * @param Swift_KeyCache            $cache
-     * @param Swift_Mime_Grammar        $grammar
-     * @param string                    $charset
-     */
-    public function __construct(Swift_Mime_HeaderSet $headers, Swift_Mime_ContentEncoder $encoder, Swift_KeyCache $cache, Swift_Mime_Grammar $grammar, $charset = null)
-    {
-        parent::__construct($headers, $encoder, $cache, $grammar);
-        $this->setContentType('text/plain');
-        if (null !== $charset) {
-            $this->setCharset($charset);
-        }
-    }
-
-    /**
-     * Set the body of this entity, either as a string, or as an instance of
-     * {@link Swift_OutputByteStream}.
+     * Details may be optionally passed into the constructor.
      *
-     * @param mixed  $body
-     * @param string $contentType optional
-     * @param string $charset     optional
-     *
-     * @return $this
-     */
-    public function setBody($body, $contentType = null, $charset = null)
-    {
-        if (isset($charset)) {
-            $this->setCharset($charset);
-        }
-        $body = $this->_convertString($body);
-
-        parent::setBody($body, $contentType);
-
-        return $this;
-    }
-
-    /**
-     * Get the character set of this entity.
-     *
-     * @return string
-     */
-    public function getCharset()
-    {
-        return $this->_getHeaderParameter('Content-Type', 'charset');
-    }
-
-    /**
-     * Set the character set of this entity.
-     *
-     * @param string $charset
-     *
-     * @return $this
-     */
-    public function setCharset($charset)
-    {
-        $this->_setHeaderParameter('Content-Type', 'charset', $charset);
-        if ($charset !== $this->_userCharset) {
-            $this->_clearCache();
-        }
-        $this->_userCharset = $charset;
-        parent::charsetChanged($charset);
-
-        return $this;
-    }
-
-    /**
-     * Get the format of this entity (i.e. flowed or fixed).
-     *
-     * @return string
-     */
-    public function getFormat()
-    {
-        return $this->_getHeaderParameter('Content-Type', 'format');
-    }
-
-    /**
-     * Set the format of this entity (flowed or fixed).
-     *
-     * @param string $format
-     *
-     * @return $this
-     */
-    public function setFormat($format)
-    {
-        $this->_setHeaderParameter('Content-Type', 'format', $format);
-        $this->_userFormat = $format;
-
-        return $this;
-    }
-
-    /**
-     * Test if delsp is being used for this entity.
-     *
-     * @return bool
-     */
-    public function getDelSp()
-    {
-        return 'yes' == $this->_getHeaderParameter('Content-Type', 'delsp') ? true : false;
-    }
-
-    /**
-     * Turn delsp on or off for this entity.
-     *
-     * @param bool $delsp
-     *
-     * @return $this
-     */
-    public function setDelSp($delsp = true)
-    {
-        $this->_setHeaderParameter('Content-Type', 'delsp', $delsp ? 'yes' : null);
-        $this->_userDelSp = $delsp;
-
-        return $this;
-    }
-
-    /**
-     * Get the nesting level of this entity.
-     *
-     * @see LEVEL_TOP, LEVEL_ALTERNATIVE, LEVEL_MIXED, LEVEL_RELATED
-     *
-     * @return int
-     */
-    public function getNestingLevel()
-    {
-        return $this->_nestingLevel;
-    }
-
-    /**
-     * Receive notification that the charset has changed on this document, or a
-     * parent document.
-     *
+     * @param string $body
+     * @param string $contentType
      * @param string $charset
      */
-    public function charsetChanged($charset)
+    public function __construct($body = null, $contentType = null, $charset = null)
     {
+        call_user_func_array(
+            array($this, 'Swift_Mime_MimePart::__construct'),
+            Swift_DependencyContainer::getInstance()
+                ->createDependenciesFor('mime.part')
+            );
+
+        if (!isset($charset)) {
+            $charset = Swift_DependencyContainer::getInstance()
+                ->lookup('properties.charset');
+        }
+        $this->setBody($body);
         $this->setCharset($charset);
-    }
-
-    /** Fix the content-type and encoding of this entity */
-    protected function _fixHeaders()
-    {
-        parent::_fixHeaders();
-        if (count($this->getChildren())) {
-            $this->_setHeaderParameter('Content-Type', 'charset', null);
-            $this->_setHeaderParameter('Content-Type', 'format', null);
-            $this->_setHeaderParameter('Content-Type', 'delsp', null);
-        } else {
-            $this->setCharset($this->_userCharset);
-            $this->setFormat($this->_userFormat);
-            $this->setDelSp($this->_userDelSp);
+        if ($contentType) {
+            $this->setContentType($contentType);
         }
     }
 
-    /** Set the nesting level of this entity */
-    protected function _setNestingLevel($level)
+    /**
+     * Create a new MimePart.
+     *
+     * @param string $body
+     * @param string $contentType
+     * @param string $charset
+     *
+     * @return self
+     */
+    public static function newInstance($body = null, $contentType = null, $charset = null)
     {
-        $this->_nestingLevel = $level;
-    }
-
-    /** Encode charset when charset is not utf-8 */
-    protected function _convertString($string)
-    {
-        $charset = strtolower($this->getCharset());
-        if (!in_array($charset, array('utf-8', 'iso-8859-1', 'iso-8859-15', ''))) {
-            // mb_convert_encoding must be the first one to check, since iconv cannot convert some words.
-            if (function_exists('mb_convert_encoding')) {
-                $string = mb_convert_encoding($string, $charset, 'utf-8');
-            } elseif (function_exists('iconv')) {
-                $string = iconv('utf-8//TRANSLIT//IGNORE', $charset, $string);
-            } else {
-                throw new Swift_SwiftException('No suitable convert encoding function (use UTF-8 as your charset or install the mbstring or iconv extension).');
-            }
-
-            return $string;
-        }
-
-        return $string;
+        return new self($body, $contentType, $charset);
     }
 }
