7d8d64856f8229f21c91458c28d6c53148e25e1b
devsmitra258 <45045327+devsmitra258@users.noreply.github.com>
2018-11-15 15:06:53 +0530

    Create app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,user-scalable=no">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1,user-scalable=no">
+        <title>Construction Map</title>
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/nihilo.css">
+        <link rel="stylesheet" href="/css/esri.css">
+        <script src="/js/arcgis.js"></script>
+        <script src="/js/cm.js"></script>
+    </head>
+    <body class="nihilo">
+        @yield('content')
+    </body>
+</html>
